815e05bd348e8bbf7bf8490f636a15d127954b93
germanpagano <sergiogermanpagano@gmail.com>
2023-05-22 02:15:56 -0300

    update

--- services/firebase.js
+++ services/firebase.js
@@ -16,3 +16,32 @@ const app = initializeApp(firebaseConfig);
 
   // Obtiene la instancia de Firestore
   export const db = getFirestore();
+
+
+
+  async function cargarHojas () {
+
+    try {
+        const hojas = [
+          { "nombre": 701, "turnos": [1, 2, 3, 4, 5, 6, 7] },
+          { "nombre": 702, "turnos": [1, 2, 3, 4, 5, 6, 7] },
+          { "nombre": 703, "turnos": [1, 2, 3, 4, 5, 6, 7] },
+          { "nombre": 704, "turnos": [1, 2, 3, 4, 5, 6, 7] },
+          { "nombre": 705, "turnos": [1, 2, 3, 4, 5, 6, 7] },
+          { "nombre": 706, "turnos": [1, 2, 3, 4, 5, 6, 7] },
+          { "nombre": 707, "turnos": [1, 2, 3, 4, 5, 6, 7] },
+          { "nombre": 708, "turnos": [1, 2, 3, 4, 5, 6, 7] },
+          { "nombre": 709, "turnos": [1, 2, 3, 4, 5, 6, 7] },
+          { "nombre": 710, "turnos": [1, 2, 3, 4, 5, 6, 7] }
+        ];
+    
+        for (const hoja of hojas) {
+          await addDoc(collection(db, "hojas"), hoja);
+        }
+    
+        console.log("Hojas cargadas exitosamente en Firebase Firestore");
+      } catch (error) {
+        console.error("Error al cargar las hojas en Firebase Firestore:", error);
+      }
+  }
+  export { cargarDatos };
