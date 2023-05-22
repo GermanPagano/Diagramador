a038229788a5c69a3130aa3b9edb0adb1ef83a30
germanpagano <sergiogermanpagano@gmail.com>
2023-05-22 02:12:00 -0300

    update

--- services/firebase.js
+++ services/firebase.js
@@ -2,14 +2,6 @@ import { initializeApp } from "firebase/app";
 import { getFirestore, collection, addDoc } from "firebase/firestore";
 
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-
- export async function cargarDatos() {
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBZyBYi6p90G4zsZuWvc6fLMAv7_rvltII",
   authDomain: "diagramador-7b12c.firebaseapp.com",
@@ -23,23 +15,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
   // Obtiene la instancia de Firestore
-  const db = getFirestore();
-
-  try {
-    const datos = [
-      { "nombre": 701, "turnos": [1, 2, 3, 4, 5, 6, 7] },
-      { "nombre": 702, "turnos": [1, 2, 3, 4, 5, 6, 7] },
-      { "nombre": 703, "turnos": [1, 2, 3, 4, 5, 6, 7] },
-      // Resto de los datos...
-    ];
-
-    for (const dato of datos) {
-      await addDoc(collection(db, "diagramas"), dato);
-    }
-
-    console.log("Datos cargados exitosamente en Firebase Firestore");
-  } catch (error) {
-    console.error("Error al cargar los datos en Firebase Firestore:", error);
-  }
-  
-}
+  export const db = getFirestore();
