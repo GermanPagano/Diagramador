1f53773c8caf5cd460365e0e243911e2375cfda4
germanpagano <sergiogermanpagano@gmail.com>
2023-05-22 02:42:13 -0300

    update

--- services/hojas-Diagramas.js
+++ services/hojas-Diagramas.js
@@ -1,12 +1,11 @@
 export const hojas = [
-  { "nombre": 701, "turnos": [1, 2, 3, 4, 5, 6, 7] },
-  { "nombre": 702, "turnos": [1, 2, 3, 4, 5, 6, 7] },
-  { "nombre": 703, "turnos": [1, 2, 3, 4, 5, 6, 7] },
-  { "nombre": 704, "turnos": [1, 2, 3, 4, 5, 6, 7] },
-  { "nombre": 705, "turnos": [1, 2, 3, 4, 5, 6, 7] },
-  { "nombre": 706, "turnos": [1, 2, 3, 4, 5, 6, 7] },
-  { "nombre": 707, "turnos": [1, 2, 3, 4, 5, 6, 7] },
-  { "nombre": 708, "turnos": [1, 2, 3, 4, 5, 6, 7] },
-  { "nombre": 709, "turnos": [1, 2, 3, 4, 5, 6, 7] },
-  { "nombre": 710, "turnos": [1, 2, 3, 4, 5, 6, 7] }
+  { nombre: 701, turnos: [1, 2, 3, 4, 5, 6, 7] },
+  { nombre: 702, turnos: [1, 2, 3, 4, 5, 6, 7] },
+  { nombre: 703, turnos: [1, 2, 3, 4, 5, 6, 7] },
+  { nombre: 704, turnos: [1, 2, 3, 4, 5, 6, 7] },
+  { nombre: 705, turnos: [1, 2, 3, 4, 5, 6, 7] },
+  { nombre: 706, turnos: [1, 2, 3, 4, 5, 6, 7] },
+  { nombre: 707, turnos: [1, 2, 3, 4, 5, 6, 7] },
+  { nombre: 708, turnos: [1, 2, 3, 4, 5, 6, 7] },
+
 ]
